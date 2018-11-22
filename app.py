73e1fdc12ba013fe227d16f0bdd0d7d378f57e78
Duong <hxhduong4394@gmail.com>
2018-11-22 17:39:52 +0700

    duong

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def result(keyword):
     for recipe in recipes:
       if ingre in recipe.ingredients_name:
           recipe_documents.append(recipe)
-  return render_template("result_p2.html", recipe_documents = recipe_documents)
+  return render_template("result_3.html", recipe_documents = recipe_documents)
 
 @app.route("/<id>")
 def display(id):
@@ -34,7 +34,7 @@ def display(id):
   return render_template("recipie.html",recipe_details=recipe_details)
 
 @app.route("/result")
-def result():
+def result_1():
   return render_template("result_3.html")
 
 if __name__ == '__main__':
